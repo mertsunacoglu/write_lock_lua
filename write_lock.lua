5272ebf42efd17ace44c03c8ef7a6c19828de8e8
Emin Sunacoglu <emin.sunacoglu@clyso.com>
2025-11-12 13:57:34 +0100

    Add working script

--- write_lock.lua
+++ write_lock.lua
@@ -1,59 +1,75 @@
--- write_lock.lua
 
-function parse_roles_from_config()
-  local roles_table = {}
-  local roles_str = rgw.get_config("rgw_keystone_accepted_admin_roles") or ""
-  rgw.log(10, "Write-Restriction: Reading roles from config string: '" .. roles_str .. "'")
-  for role in string.gmatch(roles_str, "([^,%s]+)") do
-    table.insert(roles_table, role)
-  end
-  return roles_table
+RGWDebugLog("################################ RGW WRITE-LOCK SCRIPT (V3 - FINAL) ################################")
+
+local ADMIN_USER_ID = "adminid"
+
+local write_methods = { 
+  PUT = true, 
+  POST = true, 
+  DELETE = true, 
+  COPY = true 
+}
+
+local user_id = "anonymous" -- Default to anonymous
+local is_privileged_user = false
+
+if Request and Request.User and Request.User.Id then
+  user_id = Request.User.Id
+  RGWDebugLog("Write-Restriction: Found User.Id: " .. user_id)
+else
+  RGWDebugLog("Write-Restriction: Could not find Request.User.Id. Treating as non-privileged.")
 end
 
-function has_allowed_role(user_roles, allowed_roles_list)
-  if not user_roles then
-    return false
-  end
-  for _, user_role in ipairs(user_roles) do
-    for _, allowed_role in ipairs(allowed_roles_list) do
-      if user_role == allowed_role then
-        return true
-      end
-    end
-  end
-  return false
+if user_id == ADMIN_USER_ID then
+  is_privileged_user = true
+end
+RGWDebugLog("Write-Restriction: User=" .. user_id .. " Privileged=" .. tostring(is_privileged_user))
+
+local method = nil
+if Request and Request.HTTP and Request.HTTP.Method then
+  method = Request.HTTP.Method
 end
 
-local allowed_roles = parse_roles_from_config()
-local req = rgw.request
-local write_methods = { PUT = true, POST = true, DELETE = true, COPY = true }
+if method and write_methods[method] then
+  RGWDebugLog("Write-Restriction: Is a write method (" .. method .. "). Checking permissions...")
 
-if write_methods[req:method()] then
-  
-  local user_info = req:get_user_info()
-  local user_roles = user_info["roles"]
-  local is_privileged_user = has_allowed_role(user_roles, allowed_roles)
+  if is_privileged_user then
+    RGWDebugLog("Write-Restriction: Privileged user. ALLOWING all write operations.")
+    return 0 -- Allow
+  end
 
-  rgw.log(10, "Write-Restriction: Method=" .. req:method() .. " User=" .. user_info.user_id .. " Privileged=" .. tostring(is_privileged_user))
+  RGWDebugLog("Write-Restriction: Non-privileged user. Checking restrictions...")
 
-  if not is_privileged_user then
-    local header_key_s3 = "x-amz-meta-write-restricted"
-    local header_key_swift = "x-container-meta-write-restricted"
+  local header_key = "x-amz-meta-write-restricted"
+  local header_value = nil
+  if Request.HTTP.Metadata and Request.HTTP.Metadata[header_key] then
+     header_value = Request.HTTP.Metadata[header_key]
+  end
 
-    if req:get_header(header_key_s3) or req:get_header(header_key_swift) then
-      rgw.log(4, "Write-Restriction: Non-privileged user " .. user_info.user_id .. " attempted to set restriction header on bucket " .. req:get_bucket_name())
-      return rgw.http_error("403", "Forbidden", "User is not allowed to modify the write-restricted header.")
-    end
+  if header_value ~= nil then
+    RGWDebugLog("Write-Restriction: Non-privileged user " .. user_id .. " attempted to set restriction header. ABORTING.")
+    return RGW_ABORT_REQUEST -- Block
+  end
+
+  local bucket_metadata = nil
+  if Request.Bucket and Request.Bucket.Metadata then
+    bucket_metadata = Request.Bucket.Metadata
+  end
+
+  if bucket_metadata then
+    local write_restricted = bucket_metadata["write-restricted"]
     
-    local bucket_info = req:get_bucket_info()
-    if bucket_info and bucket_info.metadata then
-      local write_restricted = bucket_info.metadata["x-amz-meta-write-restricted"]
-      if write_restricted == "true" then
-        rgw.log(4, "Write-Restriction: Blocked write by user " .. user_info.user_id .. " to restricted bucket " .. bucket_info.bucket.name)
-        return rgw.http_error("403", "Forbidden", "Writes are restricted for this container.")
-      end
+    if write_restricted == "true" then
+      RGWDebugLog("Write-Restriction: Non-privileged user " .. user_id .. " attempted to write to a restricted bucket. ABORTING.")
+      return RGW_ABORT_REQUEST -- Block
     end
   end
+
+  RGWDebugLog("Write-Restriction: Non-privileged user performing normal write to unrestricted bucket. ALLOWING.")
+  return 0 -- Allow
+
 end
 
-return nil
+-- If not a write method (e.g., GET, HEAD, LIST), allow it.
+RGWDebugLog("Write-Restriction: Not a write method. Allowing.")
+return 0
